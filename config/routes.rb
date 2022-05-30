8d65e453db15b22ee5099e35c94d741976606118
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-05-30 11:33:52 +0100

    changes to website

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
 
   # get 'lists', to: 'lists#index'
   # get 'lists/new', to: 'lists#new'
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   #   resources :bookmarks, only: [:create]
   # end
 
-  resources :lists, only: [:index, :new, :create, :show] do
+  resources :lists, only: [:new, :create, :show] do
       resources :bookmarks, only: [:new, :create]
     end
     resources :bookmarks, only: :destroy
